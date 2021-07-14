655574fe390b76db6a1c05fc0946639bab0d511d
Omar Metwally <omarsamy2011@gmail.com>
2021-07-13 20:30:50 -0400

    Finished Week9 in XML

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,39 +9,42 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
 
     String stringURL;
+    Bitmap image;
+    String current;
+    String min;
+    String max;
+    String description;
+    String iconName;
+    String humidity;
     private EditText cityTextField;
     private Button forecastButton;
-    Bitmap image;
-
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -55,10 +58,16 @@ public class MainActivity extends AppCompatActivity {
         forecastButton.setOnClickListener(v -> {
             String city = cityTextField.getText().toString();
 
+            AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Getting Forecast")
+                    .setMessage("We're calling people in " + city + " to look outside their window and tell us what's the weather like over there.")
+                    .setView(new ProgressBar(MainActivity.this))
+                    .show();
+
             try {
                 stringURL = "https://api.openweathermap.org/data/2.5/weather?q="
                         + URLEncoder.encode(city, "UTF-8")
-                        + "&appid=7e943c97096a9784391a981c4d878b22&Units=Metric";
+                        + "&appid=7e943c97096a9784391a981c4d878b22&Units=Metric&mode=xml";
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
@@ -71,32 +80,40 @@ public class MainActivity extends AppCompatActivity {
                     HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                     InputStream in = new BufferedInputStream(urlConnection.getInputStream());
 
-                    String text = (new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8)))
-                            .lines()
-                            .collect(Collectors.joining("\n"));
+                    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                    factory.setNamespaceAware(false);
+                    XmlPullParser xpp = factory.newPullParser();
+                    xpp.setInput(in, "UTF-8");
 
-                    JSONObject theDocument = new JSONObject(text);
-                    JSONObject coord = theDocument.getJSONObject("coord");
-                    JSONArray weatherArray = theDocument.getJSONArray ( "weather" );
-                    int vis = theDocument.getInt("visibility");
-                    String name = theDocument.getString( "name" );
 
-                    JSONObject position0 = weatherArray.getJSONObject(0);
-                    String description = position0.getString("description");
-                    String iconName = position0.getString("icon");
+                    while (xpp.next() != XmlPullParser.END_DOCUMENT) {
+                        switch (xpp.getEventType()) {
+                            case XmlPullParser.START_TAG:
+                                if (xpp.getName().equals("temperature")) {
+                                    current = xpp.getAttributeValue(null, "value");  //this gets the current temperature
+                                    min = xpp.getAttributeValue(null, "min"); //this gets the min temperature
+                                    max = xpp.getAttributeValue(null, "max"); //this gets the max temperature
+                                } else if (xpp.getName().equals("weather")) {
+                                    description = xpp.getAttributeValue(null, "value");  //this gets the weather description
+                                    iconName = xpp.getAttributeValue(null, "icon"); //this gets the icon name
+                                } else if (xpp.getName().equals("humidity")) {
+                                    humidity = xpp.getAttributeValue(null, "value"); //this gets the icon name
+                                }
+                                break;
+                            case XmlPullParser.END_TAG:
+                                break;
+                            case XmlPullParser.TEXT:
+                                break;
+                        }
+                    }
 
-                    JSONObject mainObject = theDocument.getJSONObject("main");
-                    double current = mainObject.getDouble("temp");
-                    double min = mainObject.getDouble("temp_min");
-                    double max = mainObject.getDouble("temp_max");
-                    double humidity = mainObject.getDouble("humidity");
 
                     File file = new File(getFilesDir(), iconName + ".png");
                     if (file.exists()) {
                         image = BitmapFactory.decodeFile(getFilesDir() + "/" + iconName + ".png");
                     } else {
                         // Download image
-                        URL imgUrl = new URL( "https://openweathermap.org/img/w/" + iconName + ".png" );
+                        URL imgUrl = new URL("https://openweathermap.org/img/w/" + iconName + ".png");
                         HttpURLConnection connection = (HttpURLConnection) imgUrl.openConnection();
                         connection.connect();
                         int responseCode = connection.getResponseCode();
@@ -106,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
                             // Store image in the memory
                             FileOutputStream fOut = null;
                             try {
-                                fOut = openFileOutput( iconName + ".png", Context.MODE_PRIVATE);
+                                fOut = openFileOutput(iconName + ".png", Context.MODE_PRIVATE);
                                 image.compress(Bitmap.CompressFormat.PNG, 100, fOut);
                                 fOut.flush();
                                 fOut.close();
@@ -116,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
 
-                    runOnUiThread( (  )  -> {
+                    runOnUiThread(() -> {
                         // Showing values on UI
                         TextView tempTextView = findViewById(R.id.tempTextView);
                         tempTextView.setText("The current temperature is: " + current);
@@ -135,16 +152,12 @@ public class MainActivity extends AppCompatActivity {
 
                         ImageView weatherImage = findViewById(R.id.weatherImage);
                         weatherImage.setImageBitmap(image);
+
+                        dialog.hide();
                     });
 
 
-
-
-
-
-
-
-                } catch (IOException | JSONException e) {
+                } catch (IOException | XmlPullParserException e) {
                     Log.e("Connection error:", e.getMessage());
                 }
             });
